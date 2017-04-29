58b015cee327421b0c9fecbd7fbb3e6a3c7dbbad
EDWIN AU <edwinau@rogers.com>
2017-04-29 15:48:06 -0400

    Normal HTML response setup success

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,28 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+  end
 
+  def show
+    @books = Book.find(params[:id])
+  end
+
+  def new
+    @books = Book.new
+  end
+
+  def edit
+    @books = Book.find(params[:id])
+  end
+
+  def create
+    @book = book.new(book_params )
+      if @book.save
+        redirect_to books_url
+      else
+        render :new
+      end
+    end
+    
 end
